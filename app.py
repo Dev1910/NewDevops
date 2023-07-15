8f8d4055c7258cf41305a6b1cb6b60ef455e1b93
Dev patel <Dev19102611@gmail.com>
2023-07-16 00:51:16 +0530

    Add counter button

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-cvb
+from flask import Flask
+
+app = Flask(__name__)
+
+# Global variable to store the visit counter
+visit_count = 0
+
+@app.route('/')
+def hello_world():
+    global visit_count
+    visit_count += 1
+    return f'Hello, World! You are visitor number {visit_count}'
+
+if __name__ == '__main__':
+    app.run()
